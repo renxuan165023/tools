e05c380310940a21b39ab6ab61feaf5c677ab626
ren xuan <renxuan@123.com>
2018-08-02 22:02:12 -0400

    first commit

---
+++ filebeat_install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+rpm --import https://packages.elastic.co/GPG-KEY-elasticsearch
+cat << EOF > /etc/yum.repos.d/elasticsearch.repo
+[elastic-6.x]
+name=Elastic repository for 6.x packages
+baseurl=https://artifacts.elastic.co/packages/6.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+autorefresh=1
+type=rpm-md
+EOF
+yum install filebeat metricbeat -y
+systemctl enable filebeat
+cat << EOF > /etc/filebeat/filebeat.yml.1
+filebeat.inputs:
+- type: log
+  paths:
+    - []
+  fields:
+    project_name: ""
+  fields_under_root: true
+
+output.elasticsearch:
+  hosts: ["https://10.1.48.7:9200"]
+  index: "filebeat-da-dbk-%{+yyyy.MM.dd}"
+  username: "logstash"
+  password: "logstash"
+  ssl.verification_mode: none
+
+setup.template.name: "filebeat-da-dbk"
+setup.template.pattern: "filebeat-da-dbk-*"
+EOF
